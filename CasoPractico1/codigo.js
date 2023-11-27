bf9bbe635569f88562447e38adb59210c21a434f
Paragon <seropa@alu.edu.gva.es>
2023-11-27 17:30:46 +0100

    Caso práctico 4

---
+++ CasoPractico1/codigo.js
@@ -0,0 +1,6 @@
+let x = parseInt(prompt("Introduzca el número 1, por favor:"));
+let y = parseInt(prompt("Introduzca el número 2, por favor:"));
+let z = x + y;
+let mensaje = `El resultado de sumar ${x} y ${y} es ${z}.
+Gracias por utilizar este programa.`
+window.alert(mensaje);
